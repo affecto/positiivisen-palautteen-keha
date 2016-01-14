3fbb3096ff1ae042c0053020d3c2feab246040e4
johannesvaltonen <johannes.valtonen@affecto.com>
2016-01-14 14:30:41 +0200

    Removed unused MVC pages.

--- Source/AngularUI/Controllers/HomeController.cs
+++ Source/AngularUI/Controllers/HomeController.cs
@@ -1,29 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
+﻿using Microsoft.AspNet.Mvc;
 
-namespace AngularUI.Controllers
+namespace PositiveFeedback.AngularUI.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
