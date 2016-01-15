56617b18c0225c7d74d13e32b980f8960b1b183f
johannesvaltonen <johannes.valtonen@affecto.com>
2016-01-15 12:52:18 +0200

    Added bower and npm packages.

---
+++ Source/AngularUI/gulpfile.js
@@ -0,0 +1,46 @@
+﻿/// <binding Clean='clean' />
+
+"use strict";
+
+var gulp = require("gulp"),
+    rimraf = require("rimraf"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify");
+
+var paths = {
+    webroot: "./"
+};
+
+paths.js = paths.webroot + "Scripts/**/*.js";
+paths.minJs = paths.webroot + "Scripts/**/*.min.js";
+paths.css = paths.webroot + "Content/**/*.css";
+paths.minCss = paths.webroot + "Content/**/*.min.css";
+paths.concatJsDest = paths.webroot + "Scripts/site.min.js";
+paths.concatCssDest = paths.webroot + "Content/site.min.css";
+
+gulp.task("clean:js", function (cb) {
+    rimraf(paths.concatJsDest, cb);
+});
+
+gulp.task("clean:css", function (cb) {
+    rimraf(paths.concatCssDest, cb);
+});
+
+gulp.task("clean", ["clean:js", "clean:css"]);
+
+gulp.task("min:js", function () {
+    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+        .pipe(concat(paths.concatJsDest))
+        .pipe(uglify())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", function () {
+    return gulp.src([paths.css, "!" + paths.minCss])
+        .pipe(concat(paths.concatCssDest))
+        .pipe(cssmin())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min", ["min:js", "min:css"]);
