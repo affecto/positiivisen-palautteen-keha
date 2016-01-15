0ffc8791ced907b0218c6f54f2795a0b1cbff374
Ville Henriksson <teettari@hotmail.com>
2016-01-15 13:22:10 +0200

    Employee synchronization console application with two hard-coded employees.

---
+++ Source/Store.MongoDb/FeedbackRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Affecto.PositiveFeedback.Application;
+
+namespace Affecto.PositiveFeedback.Store.MongoDb
+{
+    internal class FeedbackRepository : IFeedbackRepository
+    {
+        public bool HasEmployee(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddEmployee(Guid id, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEmployee(Guid id, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
