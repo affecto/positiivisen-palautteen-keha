3cfc1893418d8d96af379860f3d0fcafd5bd234c
johannesvaltonen <johannes.valtonen@affecto.com>
2016-01-15 14:26:11 +0200

    Added Api project.

---
+++ Source/Api/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using Affecto.PositiveFeedback.Api;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Microsoft.Owin;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace Affecto.PositiveFeedback.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+
+            //builder.RegisterType<Log4NetLoggerFactory>().As<ILoggerFactory>();
+
+            IContainer container = builder.Build();
+
+            HttpConfiguration config = new HttpConfiguration
+            {
+                DependencyResolver = new AutofacWebApiDependencyResolver(container)
+            };
+
+            ConfigureWebApi(config);
+            app.UseAutofacWebApi(config);
+#if(DEBUG)
+            app.UseCors(CorsOptions.AllowAll);
+#endif
+
+            app.UseWebApi(config);
+
+            config.EnsureInitialized();
+        }
+
+        private static void ConfigureWebApi(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            JsonMediaTypeFormatter jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
