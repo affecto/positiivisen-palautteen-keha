1485e5504082589239bd452cb1b93b604a61474f
Ville Henriksson <teettari@hotmail.com>
2016-01-15 13:22:10 +0200

    Employee synchronization console application with two hard-coded employees.

---
+++ Source/Application/IFeedbackRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Affecto.PositiveFeedback.Application
+{
+    public interface IFeedbackRepository
+    {
+        bool HasEmployee(Guid id);
+        void AddEmployee(Guid id, string name);
+        void UpdateEmployee(Guid id, string name);
+    }
+}
---
+++ Source/Store.MongoDb/StoreModule.cs
@@ -0,0 +1,15 @@
+﻿using Affecto.PositiveFeedback.Application;
+using Autofac;
+
+namespace Affecto.PositiveFeedback.Store.MongoDb
+{
+    public class StoreModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterType<FeedbackRepository>().As<IFeedbackRepository>();
+        }
+    }
+}
