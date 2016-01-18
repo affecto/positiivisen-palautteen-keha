4107559210eef7f6a155784d1ab98c06a05cb3d6
Mika Taimen <mika.taimen@affecto.com>
2016-01-18 15:40:25 +0200

    MongoDriver added.

--- Source/Store.MongoDb/FeedbackRepository.cs
+++ Source/Store.MongoDb/FeedbackRepository.cs
@@ -1,23 +1,48 @@
 ﻿using System;
+using System.Configuration;
 using Affecto.PositiveFeedback.Application;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
 
 namespace Affecto.PositiveFeedback.Store.MongoDb
 {
     internal class FeedbackRepository : IFeedbackRepository
     {
+        protected static IMongoClient client;
+        protected static IMongoCollection<BsonDocument> collection;
+        
+        public FeedbackRepository()
+        {
+            MongoUrl mongourl = new MongoUrl(ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString);
+            client = new MongoClient(mongourl);
+            IMongoDatabase database = client.GetDatabase(mongourl.DatabaseName);
+            collection = database.GetCollection<BsonDocument>("Employee");
+        }
+        
         public bool HasEmployee(Guid id)
         {
-            throw new NotImplementedException();
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", id);
+            BsonDocument results = collection.Find(filter).FirstOrDefault();
+            return results != null;
         }
 
-        public void AddEmployee(Guid id, string name)
+        public void AddEmployee(Guid userId, string name)
         {
-            throw new NotImplementedException();
+            var document = new BsonDocument
+            {
+                {"_id", userId },
+                {"Name", name }
+            };
+            collection.InsertOne(document);
         }
 
         public void UpdateEmployee(Guid id, string name)
         {
-            throw new NotImplementedException();
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", id);
+            var update = Builders<BsonDocument>.Update
+                .Set("Name", name);
+            collection.UpdateOne(filter, update);
         }
     }
 }
