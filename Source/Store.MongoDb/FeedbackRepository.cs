5faf7999cabea40dde2cfd88853e9fa67bc90c52
Ville Henriksson <teettari@hotmail.com>
2016-01-19 09:03:51 +0200

    Simplify feedback repository.

--- Source/Store.MongoDb/FeedbackRepository.cs
+++ Source/Store.MongoDb/FeedbackRepository.cs
@@ -4,27 +4,24 @@ using Affecto.PositiveFeedback.Application;
 using MongoDB.Bson;
 using MongoDB.Driver;
 
-
 namespace Affecto.PositiveFeedback.Store.MongoDb
 {
     internal class FeedbackRepository : IFeedbackRepository
     {
-        protected static IMongoClient client;
-        protected static IMongoCollection<BsonDocument> collection;
+        private readonly IMongoCollection<BsonDocument> employees;
         
         public FeedbackRepository()
         {
             MongoUrl mongourl = new MongoUrl(ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString);
-            client = new MongoClient(mongourl);
+            IMongoClient client = new MongoClient(mongourl);
             IMongoDatabase database = client.GetDatabase(mongourl.DatabaseName);
-            collection = database.GetCollection<BsonDocument>("Employee");
+            employees = database.GetCollection<BsonDocument>("Employee");
         }
         
         public bool HasEmployee(Guid id)
         {
-            var filter = Builders<BsonDocument>.Filter.Eq("_id", id);
-            BsonDocument results = collection.Find(filter).FirstOrDefault();
-            return results != null;
+            FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("_id", id);
+            return employees.Find(filter).Any();
         }
 
         public void AddEmployee(Guid userId, string name)
@@ -34,15 +31,14 @@ namespace Affecto.PositiveFeedback.Store.MongoDb
                 {"_id", userId },
                 {"Name", name }
             };
-            collection.InsertOne(document);
+            employees.InsertOne(document);
         }
 
         public void UpdateEmployee(Guid id, string name)
         {
-            var filter = Builders<BsonDocument>.Filter.Eq("_id", id);
-            var update = Builders<BsonDocument>.Update
-                .Set("Name", name);
-            collection.UpdateOne(filter, update);
+            FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("_id", id);
+            UpdateDefinition<BsonDocument> update = Builders<BsonDocument>.Update.Set("Name", name);
+            employees.UpdateOne(filter, update);
         }
     }
 }
