12449e845896d66ccc20e5dbc51aa024faa8ff9d
Ville Henriksson <teettari@hotmail.com>
2016-01-19 15:22:16 +0200

    Unit tests for Api method GetEmployees and EmployeeMapper.

--- Source/Api/MapperFactory.cs
+++ Source/Api/MapperFactory.cs
@@ -4,7 +4,7 @@ namespace Affecto.PositiveFeedback.Api
 {
     public class MapperFactory
     {
-        public IMapper<Application.Employee, Employee> CreateEmployeeMapper()
+        public virtual IMapper<Application.Employee, Employee> CreateEmployeeMapper()
         {
             return new EmployeeMapper();
         }
