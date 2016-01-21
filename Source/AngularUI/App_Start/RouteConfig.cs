e39bb227c2357fa36739a256fe8e87393485b69a
johannesvaltonen <johannes.valtonen@affecto.com>
2016-01-21 13:43:53 +0200

    Simplified MVC structure.

--- Source/AngularUI/App_Start/RouteConfig.cs
+++ Source/AngularUI/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace Affecto.PositiveFeedback.AngularUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Index", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
