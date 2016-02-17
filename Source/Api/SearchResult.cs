2a77506a259dcff195ff9c4bd2c373dbe45a9332
johannesvaltonen <johannes.valtonen@affecto.com>
2016-02-17 15:57:28 +0200

    Added a wrapper class for search results.

---
+++ Source/Api/SearchResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Affecto.PositiveFeedback.Api
+{
+    public class SearchResult
+    {
+        public string SearchCriteria { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
