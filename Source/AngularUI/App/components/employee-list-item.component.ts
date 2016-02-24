fd8ce34b92e73756488b9ccc4164e4030fd28692
Ville Henriksson <teettari@hotmail.com>
2016-02-24 09:39:01 +0200

    Clear employee search field when selecting an employee.

--- Source/AngularUI/App/components/employee-list-item.component.ts
+++ Source/AngularUI/App/components/employee-list-item.component.ts
@@ -1,12 +1,12 @@
 ﻿import {Component} from "angular2/core";
 import {OnInit, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked} from "angular2/core";
-import {ROUTER_DIRECTIVES} from "angular2/router";
+import {RouteParams, Router, ROUTER_DIRECTIVES} from "angular2/router";
 
 @Component({
     selector: "employee-list-item",
     styles: [],
     templateUrl: "app/components/employee-list-item.html",
-    inputs: ["employee", "employeePictureUrl"],
+    inputs: ["employee", "employeePictureUrl", "employeeSearchInput"],
     directives: [ROUTER_DIRECTIVES]
 })
 
@@ -14,9 +14,19 @@ export class EmployeeListItemComponent
 {
     public employee: Employee;
     public employeePictureUrl: string;
+    public employeeSearchInput: HTMLInputElement;
 
-    public ngOnInit(): void {
+    constructor(private router: Router)
+    {
     }
 
+    public ngOnInit(): void 
+    {
+    }
 
+    public goToEmployee()
+    {
+        this.employeeSearchInput.value = "";
+        this.router.navigate(["EmployeeDetail", { id: this.employee.id }]);
+    }
 }
